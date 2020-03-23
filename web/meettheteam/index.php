a0baeeac2ec48865530f2981f6961c9ea8ac223e
seanelevan <36710179+seanelevan@users.noreply.github.com>
2020-03-23 15:07:22 -0700

    Create index.php

---
+++ web/meettheteam/index.php
@@ -0,0 +1,29 @@
+<?php
+
+include "../header.php";
+
+?>
+
+<div style="position:absolute;top:139px;left:0px;width:100%;text-align:center;background-color:#f5f5f5;">
+
+<div style="margin:auto;margin-top:30px;margin-bottom:30px;width:800px;text-align:left:">
+
+<div style="margin:auto;width:800px;background-color:#FFFFFF;box-shadow:10px 0px 30px rgba(0, 0, 0, 0.1);">
+
+<div style="padding:45px;">
+
+<div style="font-family:'Montserrat';font-weight:700;font-size:36px;color:#2CAFE4;text-align:left;">Our Mission</div>
+
+<div style="margin-top:22px;font-family:'Vollkorn';font-weight:400;font-size:22px;line-height:30px;text-align:left;color:#444444;">
+  
+  Created in memory of Iftach Yavetz Z”L, mentor to all of us at the Israeli Trade Mission and giant in the field of Israeli Consulting, <span style="color:#2cafe4;">Iftach</span>’s mission is to work in conjunction with the Israeli Consulate General in NYC to provide consulting services to Israeli startups looking to expand into the North American consumer market. We capitalize on our diverse team to infuse our entrepreneurial advisory with a fact-based, in-depth understanding of American culture and business environment. Israeli innovation deserves an American audience—and <span style="color:#2cafe4;">Iftach</span> enables its clients to chart out new territories and turn risk and compliance into opportunities.
+  
+  <a href="http://iftach.herokuapp.com/#inquire" style="text-decoration:none;display:block;margin-top:45px;"><span class="bluebutton">LET'S TALK</span></a>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
