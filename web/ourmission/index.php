ae51417a4f3ea0248b6c303449af613ca01c679a
seanelevan <36710179+seanelevan@users.noreply.github.com>
2020-03-23 14:42:29 -0700

    Update index.php

--- web/ourmission/index.php
+++ web/ourmission/index.php
@@ -4,9 +4,9 @@ include "../header.php";
 
 ?>
 
-<div style="position:absolute;top:139px;left:0px;width:100%;text-align:center;">
+<div style="position:absolute;top:136px;left:0px;width:100%;text-align:center;background-color:#f5f5f5;">
 
-<div style="margin:auto;margin-top:30px;width:800px;text-align:left:">
+<div style="margin:auto;margin-top:30px;margin-bottom:30px;width:800px;text-align:left:">
 
 <div style="margin:auto;width:800px;background-color:#FFFFFF;box-shadow:10px 0px 30px rgba(0, 0, 0, 0.1);">
 
