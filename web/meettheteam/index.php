6dea2c17612efecce60ed0c8de7dabb458bf12f5
seanelevan <36710179+seanelevan@users.noreply.github.com>
2020-03-23 15:12:11 -0700

    Update index.php

--- web/meettheteam/index.php
+++ web/meettheteam/index.php
@@ -8,22 +8,16 @@ include "../header.php";
 
 <div style="margin:auto;margin-top:30px;margin-bottom:30px;width:800px;text-align:left:">
 
-<div style="margin:auto;width:800px;background-color:#FFFFFF;box-shadow:10px 0px 30px rgba(0, 0, 0, 0.1);">
-
-<div style="padding:45px;">
-
-<div style="font-family:'Montserrat';font-weight:700;font-size:36px;color:#2CAFE4;text-align:left;">Our Mission</div>
-
-<div style="margin-top:22px;font-family:'Vollkorn';font-weight:400;font-size:22px;line-height:30px;text-align:left;color:#444444;">
+<?php
   
-  Created in memory of Iftach Yavetz Z”L, mentor to all of us at the Israeli Trade Mission and giant in the field of Israeli Consulting, <span style="color:#2cafe4;">Iftach</span>’s mission is to work in conjunction with the Israeli Consulate General in NYC to provide consulting services to Israeli startups looking to expand into the North American consumer market. We capitalize on our diverse team to infuse our entrepreneurial advisory with a fact-based, in-depth understanding of American culture and business environment. Israeli innovation deserves an American audience—and <span style="color:#2cafe4;">Iftach</span> enables its clients to chart out new territories and turn risk and compliance into opportunities.
+$teammembers = array("Jonathan Karten", "Ari Holzhauer", "David Heagy", "Phil Shnayderman", "Thomas Li", "Noa Amzallag", "Hobart Pao", "Abigale Wang", "Sean Le Van");
   
-  <a href="http://iftach.herokuapp.com/#inquire" style="text-decoration:none;display:block;margin-top:45px;"><span class="bluebutton">LET'S TALK</span></a>
-
-</div>
-
-</div>
-
-</div>
-
+foreach($teammembers as $team){
+  
+  echo '<div style="margin:auto;width:800px;background-color:#FFFFFF;box-shadow:10px 0px 30px rgba(0, 0, 0, 0.1);"> <div style="padding:30px;"> <div style="font-family:\'Montserrat\';font-weight:700;font-size:36px;color:#2CAFE4;text-align:left;">".$team."</div> <div style="margin-top:22px;font-family:\'Vollkorn\';font-weight:400;font-size:18px;line-height:28px;text-align:left;color:#444444;">Member of Iftach Executive Board</div> </div> </div> </div>';
+  
+}
+  
+?>
+  
 </div>
