a371278266962cdedb766e7998869217d7125ea7
seanelevan <36710179+seanelevan@users.noreply.github.com>
2020-03-23 13:23:49 -0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,1 +1,154 @@
-dfg
+
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700|Vollkorn&display=swap" rel="stylesheet">
+
+<title>Iftach Consulting Group</title>
+
+<style>
+
+.bluebutton{
+
+font-family:'Montserrat';font-weight:700;font-size:16px;letter-spacing:1px;color:#FFFFFF;background-color:#2cafe4;padding:20px;padding-left:50px;padding-right:50px;box-shadow:0px 10px 30px #CDECF9;transition:all 0.4s;cursor:pointer;
+
+}
+
+.bluebutton:hover{
+
+background-color:#CDECF9;
+
+color:#1993c4;
+
+}
+
+::placeholder{
+color:#999;
+}
+
+</style>
+
+
+<div style="position:absolute;top:0px;left:0px;width:100%;height:94px;">
+
+<div style="position:absolute;right:164px;top:28px;">
+
+<div class="bluebutton" style="color:#2CAFE4;background-color:#FFFFFF;padding:13px;padding-left:30px;padding-right:30px;box-shadow:inset 0px 0px 0px 1px #2cafe4, 0px 10px 30px -10px #CDECF9;font-size:12px;letter-spacing:0.4px;float:right;">LETS TALK</div>
+
+
+<div style="float:right;margin-top:12px;margin-right:20px;height:20px;width:1px;background-color:#c4c4c4;"></div>
+
+<input type="text" style="float:right;outline:0;width:160px;border:0px;background-color:#EEEEEE;border-radius:2px;padding:10px;padding-left:40px;padding-right:16px;font-family:'Montserrat';font-size:11px;font-weight:600;color:#111;margin-right:20px;margin-top:4px;background-image:url('https://i.imgur.com/Et45qZ1.png');background-size:33px;background-repeat:no-repeat;" placeholder="Search">
+
+
+<div style="float:right;margin-top:12px;margin-right:20px;height:20px;width:1px;background-color:#c4c4c4;"></div>
+
+<img src="https://i.imgur.com/PCY2CbG.png" style="height:30px;float:right;margin-right:15px;margin-top:6px;">
+
+
+<div style="float:right;margin-top:12px;margin-right:20px;height:20px;width:1px;background-color:#c4c4c4;"></div>
+
+<img src="https://i.imgur.com/37xQE9q.png" style="height:30px;float:right;margin-right:15px;margin-top:6px;">
+
+</div>
+
+<div style="padding-left:164px;padding-top:15px;">
+<img src="https://i.imgur.com/leyombT.png" style="height:64px;">
+
+</div>
+
+</div>
+
+<div style="position:absolute;top:94px;left:0px;width:100%;background-color:#EEEEEE;height:43px;border-bottom:3px solid #CCCCCC;">
+
+<div style="padding-left:164px;padding-right:164px;">
+
+<div style="color:#2cafe4;font-weight:600;font-family:'Montserrat';font-size:13px;border-bottom:4px solid #2cafe4;padding:7px;padding-top:0px;line-height:17px;float:left;text-align:center;letter-spacing:0.6px;"><span style="font-size:24px;">&bull;</span><br>Home</div>
+</div>
+
+<div style="float:left;margin-top:14px;margin-left:15px;height:20px;width:1px;background-color:#c4c4c4;"></div>
+
+<div style="color:#999999;font-weight:600;font-family:'Montserrat';font-size:13px;padding:16px;line-height:17px;float:left;text-align:center;letter-spacing:0.6px;">Our Mission</div>
+
+<div style="float:left;margin-top:14px;height:20px;width:1px;background-color:#c4c4c4;"></div>
+
+<div style="color:#999999;font-weight:600;font-family:'Montserrat';font-size:13px;padding:16px;line-height:17px;float:left;text-align:center;letter-spacing:0.6px;">Meet the Team</div>
+
+</div>
+
+</div>
+
+<div style="position:absolute;top:139px;left:0px;width:100%;height:2460px;background-image:url('http://www.pluralvote.com/BackGround.png');background-position:center;background-size:1366px;">
+
+<div style="margin-top:280px;text-align:center;">
+
+<div style="padding:16px;padding-top:24px;padding-bottom:24px;width:48px;margin:auto;border-radius:100%;border:2px solid #FFFFFF;transition:all 0.5s;top:0px;position:relative;transform:scale(0.6);" id="flourish">
+<img src="https://i.imgur.com/ItJRh1j.png" style="width:48px;">
+</div>
+
+<div style="margin:auto;margin-top:800px;width:700px;">
+
+<div style="font-family:'Montserrat';font-weight:700;font-size:36px;color:#2CAFE4;text-align:left;">Bringing Israeli companies to the North American market</div>
+
+<div style="margin-top:22px;font-family:'Vollkorn';font-weight:400;font-size:24px;line-height:30px;text-align:left;color:#444444;">We offer targeted, innovative market research and strategic insights tailored to the needs of Israeli companies hoping to penetrate the American market. Israeli innovation deserves an American audience — <span style="color:#2cafe4;">Iftach</span> is here to enable its clients to chart out new territories and turn risk & compliance into opportunities.
+
+
+<br><br><br>
+
+<span class="bluebutton">OUR MISSION</span><span class="bluebutton" style="color:#2CAFE4;background-color:#FFFFFF;padding:20px;padding-left:50px;padding-right:50px;box-shadow:inset 0px 0px 0px 2px #2cafe4, 0px 10px 30px #CDECF9;position:relative;left:15px;">CAPABILITIES</span>
+
+</div>
+
+<div style="margin:auto;margin-top:290px;width:700px;background-color:#FFFFFF;box-shadow:10px 0px 30px rgba(0, 0, 0, 0.1);height:350px;">
+
+<div style="padding:30px;">
+
+<div style="font-family:'Montserrat';font-weight:700;font-size:30px;color:#444444;">Inquire about our Services</div>
+
+<div style="font-family:'Montserrat';font-weight:700;font-size:14px;color:#444444;margin-top:30px;text-align:left;">Name
+<br><br>
+<input type="text" style="outline:0;width:100%;border:0px;background-color:#EEEEEE;border-radius:2px;padding:15px;font-family:'Montserrat';font-size:13px;font-weight:500;color:#111;"></div>
+
+
+<div style="font-family:'Montserrat';font-weight:700;font-size:14px;color:#444444;margin-top:30px;text-align:left;">Subject
+<br><br>
+<input type="text" style="outline:0;width:100%;border:0px;background-color:#EEEEEE;border-radius:2px;padding:15px;font-family:'Montserrat';font-size:13px;font-weight:500;color:#111;"></div>
+
+<div style="margin-top:45px;">
+
+<span class="bluebutton" style="border-radius:100px;">SUBMIT</span>
+
+</div>
+
+</div>
+
+</div>
+
+
+
+</div>
+
+
+</div>
+
+</div>
+
+<script>
+function flourish(){
+
+var property = document.getElementById("flourish").style.top;
+
+if(property == "0px"){
+
+document.getElementById("flourish").style.top="8px";
+
+}
+
+else{
+
+document.getElementById("flourish").style.top="0px";
+
+}
+
+
+}
+
+setInterval("flourish()", 500);
+</script>
