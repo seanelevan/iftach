984d269147bf166624afef60062764615967bb20
seanelevan <36710179+seanelevan@users.noreply.github.com>
2020-03-23 14:36:15 -0700

    Create index.php

---
+++ web/ourmission/index.php
@@ -0,0 +1,23 @@
+<?php
+
+include "../header.php";
+
+?>
+
+<div style="position:absolute;top:139px;left:0px;width:100%;text-align:center;">
+
+<div style="margin:auto;margin-top:30px;width:800px;text-align:left:">
+
+<div style="margin:auto;width:800px;background-color:#FFFFFF;box-shadow:10px 0px 30px rgba(0, 0, 0, 0.1);">
+
+<div style="padding:30px;">
+
+<div style="font-family:'Montserrat';font-weight:700;font-size:36px;color:#2CAFE4;text-align:left;">Bringing Israeli companies to the North American market</div>
+
+<div style="margin-top:22px;font-family:'Vollkorn';font-weight:400;font-size:24px;line-height:30px;text-align:left;color:#444444;">We offer targeted, innovative market research and strategic insights tailored to the needs of Israeli companies hoping to penetrate the American market. Israeli innovation deserves an American audience — <span style="color:#2cafe4;">Iftach</span> is here to enable its clients to chart out new territories and turn risk &amp; compliance into opportunities.
+
+</div>
+
+</div>
+
+</div>
