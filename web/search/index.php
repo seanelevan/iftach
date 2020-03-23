4b9d572071d769f86f7e860eb60082c63e0f9326
seanelevan <36710179+seanelevan@users.noreply.github.com>
2020-03-23 14:55:23 -0700

    Create index.php

---
+++ web/search/index.php
@@ -0,0 +1,25 @@
+<?php
+
+include "../header.php";
+
+?>
+
+<title>Iftach - Search</title>
+
+<div style="position:absolute;top:139px;left:0px;width:100%;text-align:center;background-color:#f5f5f5;">
+
+<div style="margin:auto;margin-top:30px;margin-bottom:30px;width:800px;text-align:left:">
+
+<div style="margin:auto;width:800px;background-color:#FFFFFF;box-shadow:10px 0px 30px rgba(0, 0, 0, 0.1);">
+
+<div style="padding:45px;">
+
+<div style="font-family:'Montserrat';font-weight:700;font-size:34px;color:#2CAFE4;text-align:left;">No results for <?php echo $_GET[q] ?></div>
+
+  <a href="http://iftach.herokuapp.com/" style="text-decoration:none;display:block;margin-top:45px;"><span class="bluebutton">Return to home</span></a>
+
+</div>
+
+</div>
+
+</div>
