73b8f89774260849668aa83db11435f490f540ce
seanelevan <36710179+seanelevan@users.noreply.github.com>
2020-03-23 14:23:42 -0700

    Create header.php

---
+++ web/header.php
@@ -0,0 +1,75 @@
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700|Vollkorn&display=swap" rel="stylesheet">
+
+<title>Iftach Consulting Group</title>
+
+<style>
+
+.bluebutton{
+
+font-family:'Montserrat';font-weight:700;font-size:16px;letter-spacing:1px;color:#FFFFFF;background-color:#2cafe4;padding:20px;padding-left:50px;padding-right:50px;box-shadow:0px 10px 30px #CDECF9;transition:all 0.4s;cursor:pointer;
+
+}
+
+.bluebutton:hover{
+
+background-color:#CDECF9;
+
+color:#1993c4;
+
+}
+
+::placeholder{
+color:#999;
+}
+
+</style>
+
+
+<div style="position:absolute;top:0px;left:0px;width:100%;height:94px;">
+
+<div style="position:absolute;right:164px;top:28px;">
+
+<a href="../#info" style="text-decoration:none;"><div class="bluebutton" style="color:#2CAFE4;background-color:#FFFFFF;padding:13px;padding-left:30px;padding-right:30px;box-shadow:inset 0px 0px 0px 1px #2cafe4, 0px 10px 30px -10px #CDECF9;font-size:12px;letter-spacing:0.4px;float:right;'>LETS TALK</div></a>
+
+
+<div style="float:right;margin-top:12px;margin-right:20px;height:20px;width:1px;background-color:#c4c4c4;"></div>
+
+<form action="../search" style="display:inline;"><input type="text" style="float:right;outline:0;width:160px;border:0px;background-color:#EEEEEE;border-radius:2px;padding:10px;padding-left:40px;padding-right:16px;font-family:'Montserrat';font-size:11px;font-weight:600;color:#111;margin-right:20px;margin-top:4px;background-image:url('https://i.imgur.com/Et45qZ1.png');background-size:33px;background-repeat:no-repeat;" name="q" placeholder="Search"><input type="submit" style="display:none;"></form>
+
+
+<div style="float:right;margin-top:12px;margin-right:20px;height:20px;width:1px;background-color:#c4c4c4;"></div>
+
+<a href="https://www.facebook.com/IftachGroup/" style="text-decoration:none;"><img src="https://i.imgur.com/PCY2CbG.png" style="height:30px;float:right;margin-right:15px;margin-top:6px;"></a>
+
+
+<div style="float:right;margin-top:12px;margin-right:20px;height:20px;width:1px;background-color:#c4c4c4;display:none;"></div>
+
+<img src="https://i.imgur.com/37xQE9q.png" style="height:30px;float:right;margin-right:15px;margin-top:6px;display:none;">
+
+</div>
+
+<div style="padding-left:164px;padding-top:15px;">
+  <a href="../" style="text-decoration:none;"><img src="https://i.imgur.com/leyombT.png" style="height:64px;"></a>
+
+</div>
+
+</div>
+
+<div style="position:absolute;top:94px;left:0px;width:100%;background-color:#EEEEEE;height:43px;border-bottom:3px solid #CCCCCC;">
+
+<div style="padding-left:164px;padding-right:164px;">
+
+<a href="../"><div style="color:#2cafe4;font-weight:600;font-family:'Montserrat';font-size:13px;border-bottom:4px solid #2cafe4;padding:7px;padding-top:0px;line-height:17px;float:left;text-align:center;letter-spacing:0.6px;"><span style="font-size:24px;">&bull;</span><br>Home</div></a>
+</div>
+
+<div style="float:left;margin-top:14px;margin-left:15px;height:20px;width:1px;background-color:#c4c4c4;"></div>
+
+<a href="../ourmission"><div style="color:#999999;font-weight:600;font-family:'Montserrat';font-size:13px;padding:16px;line-height:17px;float:left;text-align:center;letter-spacing:0.6px;">Our Mission</div></a>
+
+<div style="float:left;margin-top:14px;height:20px;width:1px;background-color:#c4c4c4;"></div>
+
+<a href="../meettheteam"><div style="color:#999999;font-weight:600;font-family:'Montserrat';font-size:13px;padding:16px;line-height:17px;float:left;text-align:center;letter-spacing:0.6px;">Meet the Team</div></a>
+
+</div>
+
+</div>
