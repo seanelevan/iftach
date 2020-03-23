2df0b38958a910655a02f2d8c0804381fc3ac7d5
seanelevan <36710179+seanelevan@users.noreply.github.com>
2020-03-23 13:41:26 -0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,3 @@
-<script async src="https://cse.google.com/cse.js?cx=009301861100928635810:sudw51wdu3m"></script>
-<div class="gcse-searchbox-only"></div>
 <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700|Vollkorn&display=swap" rel="stylesheet">
 
 <title>Iftach Consulting Group</title>
@@ -36,7 +34,7 @@ color:#999;
 
 <div style="float:right;margin-top:12px;margin-right:20px;height:20px;width:1px;background-color:#c4c4c4;"></div>
 
-<input type="text" style="float:right;outline:0;width:160px;border:0px;background-color:#EEEEEE;border-radius:2px;padding:10px;padding-left:40px;padding-right:16px;font-family:'Montserrat';font-size:11px;font-weight:600;color:#111;margin-right:20px;margin-top:4px;background-image:url('https://i.imgur.com/Et45qZ1.png');background-size:33px;background-repeat:no-repeat;" placeholder="Search">
+<form action="https://cse.google.com/cse" style="display:inline"><inpu type="text" name="cse" value="009301861100928635810:sudw51wdu3m" style="display:none;"><input type="text" style="float:right;outline:0;width:160px;border:0px;background-color:#EEEEEE;border-radius:2px;padding:10px;padding-left:40px;padding-right:16px;font-family:'Montserrat';font-size:11px;font-weight:600;color:#111;margin-right:20px;margin-top:4px;background-image:url('https://i.imgur.com/Et45qZ1.png');background-size:33px;background-repeat:no-repeat;" name="q" placeholder="Search"></form>
 
 
 <div style="float:right;margin-top:12px;margin-right:20px;height:20px;width:1px;background-color:#c4c4c4;"></div>
